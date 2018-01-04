2cc5a60ff4e6209a33baffea9a9352f0420fb935
Appie Verschoor <xiffy@solr.1>
2018-01-04 01:25:15 +0100

    formatting and some click-through options

--- app.py
+++ app.py
@@ -15,16 +15,16 @@ from kb.nl.helpers import alto_to_text
 
 sources = SETS.keys()
 app = Flask(__name__)
-app.jinja_env.undefined = jinja2.StrictUndefined
+if app.debug:
+     app.jinja_env.undefined = jinja2.StrictUndefined
 
 @app.route("/")
 def binnen():
-    payload = render_template('home.html', sources=sources)
+    payload = render_template('home.html', sources=sorted(sources))
     return payload
 
 @app.route("/vind")
 def vinden():
-    print (request)
     source = request.args.get('bron')
     q = request.args.get('q')
     if q and source:
@@ -32,24 +32,39 @@ def vinden():
         q = (' AND ').join(q)
 
     q_response = sru.search(q, source)
-    oai_handler = oai
-    oai_handler.__init__(current_set=source)
-    results = []
+    if q_response:
+        oai_handler = oai
+        oai_handler.__init__(current_set=source)
+        results = []
+        i = 0
 
-    for record in q_response.records:
-        if not record.identifiers:
-            continue
-        r = oai_handler.get(record.identifiers[0])
-        if r.alto:
-            for alto in r.alto:
-                if alto:
-                    record.fulltext = alto_to_text(alto)
-                else:
-                    record.fulltext = None
-        results.append(record)
+        for record in q_response.records:
+            if not record.identifiers:
+                continue
+            result = {}
+            i += 1
+            if i < 10:
+                r = oai_handler.get(record.identifiers[0])
+                if r.alto:
+                    for alto in r.alto:
+                        if alto:
+                            result['fulltext'] = alto_to_text(alto)
+                        else:
+                            result['fulltext'] = None
 
-    payload = render_template('home.html',
-                               sources=sources,
-                               response = q_response,
-                               results=results)
+            result['dates'] = ' '.join(str(el) for el in record.dates)
+            result['identifiers'] = '{}{}'.format(SETS[source]['resolver'], ''.join(str(el) for el in record.identifiers))
+            result['abstracts'] = ' '.join(str(el) for el in record.abstracts)
+            result['titles'] = ' '.join(str(el) for el in record.titles)
+            results.append(result)
+
+        payload = render_template('home.html',
+                                   sources = sorted(sources),
+                                   response = q_response,
+                                   results = results,
+                                   selected = source,
+                                   q = request.args.get('q'))
+    else:
+        payload = 'Sorry, we screwed up'
     return payload
+
